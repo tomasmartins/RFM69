5f9df5f1764c6893320e9a8f67eea04340087b9e
tomasmartins <TRM@TRMMacBookPro.local>
2017-05-07 19:58:21 +0100

    KekusMaximus

--- example.py
+++ example.py
@@ -5,12 +5,8 @@ from RFM69registers import *
 import datetime
 import time
 
-test = RFM69.RFM69(RF69_915MHZ, 1, 1, True)
+test = RFM69.RFM69(RF69_868MHZ, 1, 1, True)
 print "class initialized"
-print "reading all registers"
-results = test.readAllRegs()
-for result in results:
-    print result
 print "Performing rcCalibration"
 test.rcCalibration()
 print "setting high power"
@@ -18,17 +14,10 @@ test.setHighPower(True)
 print "Checking temperature"
 print test.readTemperature(0)
 print "setting encryption"
-test.encrypt("1234567891011121")
+test.encrypt("kekusmaximus1997")
 print "sending blah to 2"
-if test.sendWithRetry(2, "blah", 3, 20):
-    print "ack recieved"
-print "reading"
 while True:
-    test.receiveBegin()
-    while not test.receiveDone():
-        time.sleep(.1)
-    print "%s from %s RSSI:%s" % ("".join([chr(letter) for letter in test.DATA]), test.SENDERID, test.RSSI)
-    if test.ACKRequested():
-        test.sendACK()
+    if test.sendWithRetry(2, "blah", 3, 20):
+        print "ack recieved"
 print "shutting down"
 test.shutdown()

819eeb535db1ebf957b417f1c8c0b99dfe7ed7dd
tomasmartins <TRM@TRMMacBookPro.local>
2017-05-08 20:59:40 +0100

    test

--- example.py
+++ example.py
@@ -19,18 +19,11 @@ print "setting encryption"
 test.encrypt("kekusmaximus1997")
 print "sending blah to 2"
 while True:
-    if test.send(2, "kekusmaximus1997"+ str(random.randint(1,1500)), True):
-        print "ack recieved"
-        time.sleep(1)
-    if test.ACKReceived(2):
-        print "ACK KEK"
-    print "Sending Random"
-        test.receiveBegin()
+    test.receiveBegin()
     while not test.receiveDone():
         time.sleep(.1)
     print "%s from %s RSSI:%s" % ("".join([chr(letter) for letter in test.DATA]), test.SENDERID, test.RSSI)
     if test.ACKRequested():
         test.sendACK()
-        
 print "shutting down"
 test.shutdown()

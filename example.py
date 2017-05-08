7c37d24b2e5e0606614537d086e35cfb920d3cb1
tomasmartins <TRM@TRMMacBookPro.local>
2017-05-08 20:40:02 +0100

    Test

--- example.py
+++ example.py
@@ -4,6 +4,8 @@ import RFM69
 from RFM69registers import *
 import datetime
 import time
+import random
+
 
 test = RFM69.RFM69(RF69_868MHZ, 1, 1, True)
 print "class initialized"
@@ -17,8 +19,9 @@ print "setting encryption"
 test.encrypt("kekusmaximus1997")
 print "sending blah to 2"
 while True:
-    if test.send(2, "blah", True):
+    if test.send(2, "kekusmaximus1997"+ random.randint(1,1500), True):
         print "ack recieved"
         time.sleep(1)
+    print "Sending Random"
 print "shutting down"
 test.shutdown()
